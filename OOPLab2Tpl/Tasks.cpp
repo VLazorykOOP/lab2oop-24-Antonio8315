f71ea6800728f3af2d750470909bc0892394b04b
Antonio8315 <berehii.anton@chnu.edu.ua>
2024-03-17 11:30:06 +0200

    1 task

--- OOPLab2Tpl/Tasks.cpp
+++ OOPLab2Tpl/Tasks.cpp
@@ -10,9 +10,35 @@ void MenuTask()
     cout << "    3.  Data encryption using structures with bit fields \n";
     cout << "    4.  The problem of using bitwise operations \n";
     cout << "    5.  Examples of problems using bitwise operations \n";
+    int num;
     cout << "    6.  Exit \n";
+    cout << " Input num \n";
+    cin >> num;
+    switch (num) {
+    case 1: task1(); break;
+    case 2: task2(); break;
+    default: cout << "No number";
+    }
 }
-void task1() {
+void task1() {  
+    int a, b, c, d;
+    int x, x1, x2;
+    std::cout << "Input a: ";
+    std::cin >> a;
+    std::cout << "Input b: ";
+    std::cin >> b;
+    std::cout << "Input c: ";
+    std::cin >> c;
+    std::cout << "Input d: ";
+    std::cin >> d;
+    x2 = (15 * a + 312 * b) / 64 - 120 * c + 121 * d;
+    x1 = ((a * 16 - a) + (b * 256) + (b * 64) - (b * 8)) / 64 - (c * 128 - c * 8) + (d * 128 - d * 8 + d);
+    x = ((((a << 4) - a) + ((b << 8) + (b << 6) - (b << 3))) >> 6) - (c << 7) + (c << 3) + (d << 7) - (d << 3) + d;
+    
+    //cout << "x1= " << x1 << "\n";
+    cout << "x= " << x << "\n";
+    cout << "x1= " << x1 << "\n";
+    cout << "x2= " << x2 << "\n";
     // Обчислення виразів з використанням побітових операцій
     // Calculation of expressions using bitwise operations 
     cout << " Calculation of expressions using bitwise operations  \n";
@@ -40,5 +66,6 @@ void task4()
     cout << " Data encryption using structures with bit fields \n";
 
 }
+
 
 
