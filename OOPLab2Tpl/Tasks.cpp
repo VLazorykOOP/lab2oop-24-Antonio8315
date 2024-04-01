986cfcfc93040230e01d49f82e2beef75c3e3f14
Antonio8315 <berehii.anton@chnu.edu.ua>
2024-04-01 11:34:18 +0300

    2 task

--- OOPLab2Tpl/Tasks.cpp
+++ OOPLab2Tpl/Tasks.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <fstream>
 using namespace std;
 #include "Tasks.h"
 #include "Examples.h"
@@ -17,6 +18,8 @@ void MenuTask()
     switch (num) {
     case 1: task1(); break;
     case 2: task2(); break;
+    case 3: task3(); break;
+    case 4: task4(); break;
     default: cout << "No number";
     }
 }
@@ -49,9 +52,123 @@ void task2()
     // Шифрування даних з використання побітових операцій 
     // Data encryption using bitwise operations
     cout << " Data encryption using bitwise operations  \n";
+    int i, j;
 
-}
 
+    char S[4][33], Shufr[4][32], Rozshufr[4][32], ch; 
+    //відкриття текстового файлу
+    ofstream ofs("in.txt");
+    if (!ofs) {
+        cout << "File not open \n";
+    }
+    if (ofs) {
+        cout << "Input text 32 / 4: \n";
+    }
+
+    //запис у текстовий файл, добавляємо пробіли
+    for (i = 0;i < 4;i++) {
+        cin.get();
+        cin.get(S[i], 33);
+        ofs << S[i];
+        int n = strlen(S[i]);
+        for (j = n; j < 32; j++) {
+            S[i][j] = '\0'; ofs << S[i][j];
+        }
+        ofs << endl;
+    }
+    
+    
+        ofs.close();
+    
+    
+    //шифрування
+    ifstream ifs("in.txt");
+    if (!ifs) {
+     cout << "File in.txt not open" << endl; 
+    }
+    else {
+        for (i = 0;i < 4;i++) {
+            ifs.get(S[i], 32);
+        }
+        unsigned char c;
+        unsigned short sh_dat[64];
+        unsigned short  l, b, k, t, r, w;
+        for (i = 0;i < 4;i++) {
+            for (j = 0;j < 32;j++) {
+                r = i;
+                w = j << 2;
+                r |= w;
+                c = S[i][j]; 
+                t = c << 6;
+                r |= t;
+
+                b = 0; t = 1;
+                for (k = 0;k < 16;k++) // обчислення біта парності
+                {
+                    if (r & t) {
+                        if (b == 0) b = 1; else b = 0;
+                    }
+                    t <<= 1;
+                }
+                r |= b << 15;
+
+
+                Shufr[i][j] = r;
+
+            }
+
+        }
+    }
+    ifs.close();
+    //виведення шифрованого тексту на екран
+    cout << "Shufr: \n";
+    for (i = 0;i < 4;i++) {
+        for (j = 0;j < 32;j++) { cout << Shufr[i][j]; }
+        cout << "\n";
+    }
+
+    unsigned short sd2[128];
+    sd2[i] = Shufr[i][j];
+    for (i = 0;i < 128;i++) {
+        cout << sd2[i];
+    }
+    ofstream ofsb("outb.bin", ios::out | ios::binary);
+    for (i = 0;i < 4;i++) {
+        ofsb.write((char*)Shufr[i][j], 32 * sizeof(unsigned short));
+    }
+    ofsb.write((char*)sd2[i], 128 * sizeof(unsigned short));
+    
+    ofs.close();
+
+    //Розшифровка
+    
+    ifs.open("outb.dat", ios::in | ios::binary);
+    short indi, indj;
+    unsigned short  l, b, k, t, r, w;
+    
+        ifs.read((char*)sd2, 128 * sizeof(unsigned short));
+    
+    for (i = 0;i < 128;i++) {
+        r = sd2[i];
+        indj = r & 0x007c; // 0000 0000 0111 1100 = 0x007c
+        indj >>= 2;
+        indi = r & 0x0003; // 0000 0000 0000 0011 = 0x0003
+        w = r & 0x7f80;    // 0111 1111 1000 0000 = 0x7f80 
+        w >>= 7;
+        ch = w;
+        Rozshufr[indi][indj] = ch;
+
+        
+    }
+
+    //Виведення розшифрованого тексту
+    cout << "Rozshufr: " << endl;
+    for (i = 0;i < 4; i++) {
+        for (j = 0;j < 32; j++)
+            cout << Rozshufr[i][j];
+        cout << endl;
+    }
+}   
 void task3()
 {
     // Шифрування даних з використання стуктур з бітовими полями 
